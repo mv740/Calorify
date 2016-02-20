2bb8915c17d62405ddd075306eec646b3b5faabd
francis cote-tremblay <franciscotetremblay@gmail.com>
2016-02-20 11:52:20 -0500

    added api for easycamera

--- Calorify/app/src/main/java/com/example/lenovo/calorify/MainActivity.java
+++ Calorify/app/src/main/java/com/example/lenovo/calorify/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lenovo.calorify;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import net.bozho.easycamera.DefaultEasyCamera;
+import net.bozho.easycamera.EasyCamera;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*EasyCamera camera = DefaultEasyCamera.open();
+        EasyCamera.CameraActions actions = camera.startPreview(surface);
+        Camera.PictureCallback callback = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, EasyCamera.CameraActions actions) {
+                // store picture
+            }
+        };
+        actions.takePicture(EasyCamera.Callbacks.create().withJpegCallback(callback));*/
     }
 }
